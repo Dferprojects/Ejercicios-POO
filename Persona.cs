e2cd6f81bb68b71bf713ed7b313cd9e8a88093d8
Leonardo Fernández Jara <101018024+Dferprojects@users.noreply.github.com>
2025-06-14 17:25:29 -0300

    Primera parte - First Part
    Crear una clase Persona y agregarle atributos
    - Nombre
    - Cédula
    - Dirección
    Luego, almacenar esos datos e imprimirlos en consola

---
+++ Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_PabRam_YT
+{
+    public class Persona
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        
+        public Persona(string cedula, string nombre, string direccion) {
+            Cedula = cedula;
+            Nombre = nombre;
+            Direccion = direccion;
+        }
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Datos de la persona: ");
+            Console.WriteLine("Nombre: " + this.Nombre);
+            Console.WriteLine("Cédula: " + this.Cedula);
+            Console.WriteLine("Dirección: " + this.Direccion);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_PabRam_YT
+{
+    /* Crear un programa que permita almacenar la siguiente información de Persona
+     * Modele una clase para ella: Cédula, Nombre y Dirección
+     * Nota: cada Dirección es una cadena de caracteres de la forma: Provincia, Cantón, Distrito
+     */
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Menu();
+            Console.ReadKey();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Bienvenido al sistema!");
+            Console.WriteLine("A continuación ingrese los datos requeridos...");
+            Console.WriteLine("--- --- --- --- ---");
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su cédula:");
+            string cedula = Console.ReadLine();
+            Console.WriteLine("Ingrese una dirección: ");
+            string direccion = Console.ReadLine();
+            
+            Persona p1 = new Persona(cedula, nombre, direccion);
+            Console.WriteLine("------- ------- -------");
+            p1.MostrarDatos();
+        }
+
+
+    }
+}
